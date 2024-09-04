a14a867c71a9d70f2011a2c6a2f69f95b8916074
MNdagu <ndagumark@gmail.com>
2024-09-04 17:24:01 +0300

    Final

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,19 @@
 def write_file(file_name, file_content):
-    pass
+    file_name = f"{file_name}.txt"
+    with open (file_name, mode='w', encoding='utf-8') as file:
+        file.write(file_content)
+        
 
 def append_file(file_name, append_content):
-    pass
+    file_name = f"{file_name}.txt"
+    with open (file_name, mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_name = f"{file_name}.txt"
+    with open (file_name, mode='r', encoding='utf-8') as file:
+        return file.read()
+
+write_file(file_name="logfile", file_content="Log 1: 5 bananas added\n")
+append_file(file_name="logfile", append_content="Log 2: 3 bananas subtracted\n")
+print(read_file(file_name="logfile"))
